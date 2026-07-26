e71ace3a1c0bf11a3b8ee43cd90e53bbd187fc45
Alibegovic Hamdija <ali964ham@gmail.com>
2026-07-26 17:52:30 +0200

    created basic files

---
+++ index.js
@@ -0,0 +1,35 @@
+const apiKey = "ea944817536862cd160f6fddb96a8c37";
+const weatherData = document.getElementById("weather-data");
+const cityInput = document.getElementById("city-input");
+const form = document.querySelector("form");
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const cityValue = cityInput.value;
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`);
+        if(!response.ok){
+            throw new Error("Network response was not 'ok'.");
+        }
+        const data = await response.json();
+
+        const temperature = Math.round(data.main.temp);
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}°C`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind speed: ${data.wind.speed}m/s`      
+
+
+        ];
+        weatherData.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="weather icon">`;
+        weatherData.querySelector(".temperature").textContent = `${temperature}`;
+        weatherData.querySelector(".description").textContent = `${description}`;
+        // weatherData.querySelector(".details").innerHTML = `<div>${details[0]}</div><div>${details[1]}</div><div>${details[2]}</div>`;
+        weatherData.querySelector(".details").innerHTML = details.map((detail)=>`<div>${detail}</div>`).join("");
+    }catch(error){}
+}
